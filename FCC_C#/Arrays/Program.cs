baf93b069e5960b5c9ca3b4ad52a4db9a7643581
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2024-01-28 10:33:37 -0500

    Arrays - Initial commit

---
+++ FCC_C#/Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] arr = {
+    "B123",
+    "C234",
+    "A345",
+    "C15",
+    "B177",
+    "G3003",
+    "C235",
+    "B179"
+};
+
+foreach (string id in arr) {
+    if (id.StartsWith("B")) {
+        Console.WriteLine(id);
+    }
+}
+
+int[] newArr = new int[3];
+newArr[0] = 5;
+newArr[1] = 6;
+newArr[2] = 7;
