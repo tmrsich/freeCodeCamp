9b74b08ab087a957d629547e28a20bbb6296657f
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2024-01-29 16:40:11 -0500

    More commit

--- FCC_C#/Test C#/obj/Debug/net8.0/Test C#.AssemblyInfo.cs
+++ FCC_C#/Test C#/obj/Debug/net8.0/Test C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1016f86eeaefbeca1252014949c96941f8852e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf93b069e5960b5c9ca3b4ad52a4db9a7643581")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
