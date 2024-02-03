30e77b86db72d237c06fbd4008d178a33dae5802
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2024-02-02 20:27:29 -0500

    Test commit

--- FCC_C#/Unit 3/Program.cs
+++ FCC_C#/Unit 3/Program.cs
@@ -1,22 +1,27 @@
-﻿// Exercise to practice boolean expressions
-string permission = "Admin|Manager";
-int level = 55;
+﻿
 
-if (permission.Contains("Admin")) {
-    if (level > 55) {
-        Console.WriteLine("Welcome, Super Admin user.");
-    }
-    else {
-        Console.WriteLine("Welcome, Admin user.");
-    }
-} else if (permission.Contains("Manager")) {
-    if (level >= 20) {
-        Console.WriteLine("Contact an Admin for access");
-    } else {
-        Console.WriteLine("You do not have sufficient privileges");
-    }
-}
 
-// Exercise to flip a coin, then print the result
-int coin = new Random().Next(1, 2+1);
-Console.WriteLine((coin == 1) ? "heads" : "tails");
+
+
+// // Exercise to practice boolean expressions
+// string permission = "Admin|Manager";
+// int level = 55;
+
+// if (permission.Contains("Admin")) {
+//     if (level > 55) {
+//         Console.WriteLine("Welcome, Super Admin user.");
+//     }
+//     else {
+//         Console.WriteLine("Welcome, Admin user.");
+//     }
+// } else if (permission.Contains("Manager")) {
+//     if (level >= 20) {
+//         Console.WriteLine("Contact an Admin for access");
+//     } else {
+//         Console.WriteLine("You do not have sufficient privileges");
+//     }
+// }
+
+// // Exercise to flip a coin, then print the result
+// int coin = new Random().Next(1, 2+1);
+// Console.WriteLine((coin == 1) ? "heads" : "tails");
