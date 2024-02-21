80aed27ccab6bae7a518739ec2419eaa3992a600
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2024-02-21 11:50:47 -0500

    Changed folder names to better reflect what they represent

--- FCC_Javascript-Algorithms-and-Data-Structures/FCC_Javascript-Algorithms-and-Data-Structures/8. Functional Programming/23. use-the-some-method-to-check-that-any-elements-in-an-array-meet-a-criteria.js
+++
@@ -1,8 +0,0 @@
-function checkPositive(arr) {
-  // Only change code below this line
-  return arr.some((val) => { return val > 0; });
-
-  // Only change code above this line
-}
-
-console.log(checkPositive([1, 2, 3, -4, 5]));
