283e9316ae780a67db4206ba6effc7105a095b6e
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2024-03-28 01:13:18 -0400

    Half way through creating methods in C# with different data types

--- C#/Unit 5/Challenges & Exercises/3. Creating methods that return values.cs
+++ C#/Unit 5/Challenges & Exercises/3. Creating methods that return values.cs
@@ -27,3 +27,92 @@ string FormatDecimal(double input)
 {
     return input.ToString().Substring(0, 5);
 }
+
+// Return numbers from methods
+double usd = 23.73;
+int vnd = UsdToVnd(usd);
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+
+vnd = 557655;
+usd = VndToUsd(vnd);
+Console.WriteLine($"${vnd} VND = ${usd} USD");
+
+int UsdToVnd(double usd) 
+{
+    int rate = 23500;
+    return (int)(rate * usd);
+}
+
+double VndToUsd(double vnd)
+{
+    int rate = 23500;
+    return vnd / rate;
+}
+
+// Method that returns a string
+string input = "snake";
+Console.WriteLine(input);
+Console.WriteLine(ReverseWord(input));
+
+// A string is technically an "array" of characters, with each element representing a character, which is why this works
+string ReverseWord(string word)
+{
+    string result = "";
+    for (int i = word.Length-1; i >= 0; i--)
+    {
+        result += word[i];
+    }
+    return result;
+}
+
+// Method to reverse words in a sentence
+string input = "This is a test sentence.";
+Console.WriteLine(input);
+Console.WriteLine(ReverseSentence(input));
+
+
+string ReverseSentence(string input) 
+{
+    string result = "";
+
+    // An array of words, where it splits each element such that it represents a word in the sentence
+    string[] words = input.Split(" ");
+    
+    // Want to iterate through all the words in the sentence, then iterate through all the letters in the sentence
+    // Can also use a foreach loop that iterates through each element in the array, and each operation is performed on that element
+    foreach (string word in words)
+    {
+        result += ReverseWord(word) + " ";
+    }
+    return result.Trim();
+}
+
+// A string is technically an "array" of characters, with each element representing a character, which is why this works
+string ReverseWord(string word)
+{
+    string result = "";
+    for (int i = word.Length-1; i >= 0; i--)
+    {
+        result += word[i];
+    }
+    return result;
+}
+
+// Return booleans from methods
+bool IsPalindrome(string word) 
+{
+    int start = 0;
+    int end = word.Length - 1;
+
+    while (start < end) 
+    {
+        if (word[start] != word[end]) 
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
+}
