c3a6517bbc952064a23d1d3976c9685056d7b9e5
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2024-03-28 15:32:50 -0400

    Creating methods that return arrays

--- C#/Unit 5/Challenges & Exercises/3. Creating methods that return values.cs
+++ C#/Unit 5/Challenges & Exercises/3. Creating methods that return values.cs
@@ -116,3 +116,78 @@ bool IsPalindrome(string word)
 
     return true;
 }
+
+// Return arrays from methods
+int target = 30;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
+
+// If no coins match, the return statement at the bottom will change 
+if (result.Length == 0) 
+{
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++) 
+    {
+        if (result[i,0] == -1) 
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
+    }
+}
+
+Console.WriteLine(result.GetLength(0));
+
+int[,] TwoCoins(int[] coins, int target) 
+{
+    /*
+        Initially set to -1 to ensure that it does not accidentally indicate an index position of a positive number,
+        which could signal a false positive in the coins whose sum is the target value, but in the end it doesn't even matter
+    
+        Result is initialized to 5 elements because we want to find a MAXIMUM of 5 pairs, 
+        thus the length of result will always be 5, regardless of the pairs that actually match
+    */
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    int count = 0;
+
+    // Iterates through all the coins in the coins array
+    /* 
+        Essentially, it will start at index 0 then iterate through all the other coins in the array to find one that makes the condition of the
+        current coin + the next available coin that adds up to the target value, currently initialized at 30.
+    */
+    for (int curr = 0; curr < coins.Length; curr++) 
+    {
+        // Iterates through all the "next" coins, or the coins in the index position of curr + 1
+        for (int next = curr + 1; next < coins.Length; next++) 
+        {
+            // If the condition is met, and the current coin + the next = target, initial result[count, 0] = curr (curr is initialized at 0) will change the -1 to a 0
+            // Additionally when this condition is met, result[count, 1] = next is result[0, 1] = next, which sets the -1 to the index position of the coin that matches
+            // If the condition is not met, it will check to see if it has reached the end of the array in order to avoid an array out of bounds exception
+            if (coins[curr] + coins[next] == target) 
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+
+            /*
+                If the count is equal to the length of the result, it returns the "result" array
+                which contains the list of indices of coins that add together to sum up to the target value
+            */
+            // If the count makes it to "5" which is the length of the results array (5 elements) then it will return that array, because we only want a maximum of 5 matches
+            if (count == result.GetLength(0)) 
+            {
+                return result;
+            }
+        }
+    }
+    /* 
+        If the count is 0 then it will return a new integer array of [0, 0], indicating that no coin pairs that add up to the target value have been found;
+        If this condition is false, it will just return the result
+    */
+    return (count == 0) ? new int[0,0] : result;
+}
