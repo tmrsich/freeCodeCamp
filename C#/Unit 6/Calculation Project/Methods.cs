3cddcf4dc433589566bb3c6e56bbcb94d2681d74
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2024-04-22 17:37:17 -0400

    Debug assets generated

---
+++ C#/Unit 6/Calculation Project/Methods.cs
@@ -0,0 +1,13 @@
+using System;
+public class Methods
+{
+    public void Test()
+    {
+        Console.WriteLine("Hello World! I am a method from a different C# file!");
+    }
+
+    public void AccessProgram()
+    {
+        Program program = new Program();
+    }
+}
--- C#/Unit 6/Calculation Project/obj/Debug/net8.0/Calculation Project.AssemblyInfo.cs
+++ C#/Unit 6/Calculation Project/obj/Debug/net8.0/Calculation Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculation Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4904cc4437e02bdd32c4d1ba9b2cbd58793feb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ba6c3dc43e1b4aa7e98796e4de64da725c98f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculation Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculation Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
